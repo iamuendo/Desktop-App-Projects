fdde0e5ab1d01a6db1892b04b306355dd68a1236
Isaac Muendo <52902936+iamuendo@users.noreply.github.com>
2023-06-06 08:04:42 +0300

    Add files via upload

---
+++ SimpleCalculator/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        TextBox3 = New TextBox()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(69, 46)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(62, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Operand 1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(69, 81)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(62, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Operand 2"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(137, 42)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(103, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(137, 77)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(103, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(55, 122)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(47, 35)
+        Button1.TabIndex = 4
+        Button1.Text = "+"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(108, 122)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(47, 35)
+        Button2.TabIndex = 5
+        Button2.Text = "-"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(164, 122)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(47, 35)
+        Button3.TabIndex = 6
+        Button3.Text = "X"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(217, 122)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(47, 35)
+        Button4.TabIndex = 7
+        Button4.Text = "/"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(137, 175)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(103, 23)
+        TextBox3.TabIndex = 9
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(69, 179)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(46, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Answer"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(321, 236)
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+End Class
